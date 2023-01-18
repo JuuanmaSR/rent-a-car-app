66133abd776549625f2d56d63661c43567d3c5d9
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-18 18:19:19 -0300

    Se agrega un path resolver
    Este se agrega para evitar tener que escribir la ruta completa de un archivo para importarlo a otro

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsConfigPaths from 'vite-jsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsConfigPaths()],
 })
