d601c88f534d3a98bf354ba316c355e940639a03
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-21 00:00:24 -0300

    Se modifica componente
    Este se modifica para mostrar ciertos mensajes de error dependiendo del estado de inicio de sesión del usuario

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,50 @@
-import {useForm} from 'react-hook-form'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 import useUser from 'hooks/useUser'
 import './styles.css'
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const { login: loginService, isLogged, isLoginLoading, hasLoginMessage } = useUser()
 
-    const {register, handleSubmit, formState: {errors}} = useForm()
-    const {login: loginService} = useUser()
+  const onSubmit = (values) => {
+    loginService(values)
+  }
 
-    const onSubmit = (values) => {
-        loginService(values)
-    }
-
-    return <>
+  return (
+    <>
+      {isLogged && <p>Felicidades ah iniciado sesión con éxito</p>}
+      {isLoginLoading && <p>Verificando credenciales</p>}
+      {hasLoginMessage && <p>{hasLoginMessage}</p>}
+      {!isLoginLoading && (
         <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('email', {required: true})} placeholder="Email"/>
-            {errors.email && <p> Email is required </p>}
-            <input type='password' {...register('password', {required: true})} placeholder="Password"/>
-            {errors.password && <p> Password is required </p>}
-            <input type="submit" />
+          <input
+            type="email"
+            {...register('email', { required: 'El campo email esta vacio' })}
+            placeholder="Email"
+          />
+          {errors.email && <p> {errors.email.message || errors.email.type} </p>}
+          <input
+            type="password"
+            {...register('password', {
+              required: 'El campo password esta vacio',
+              minLength: {
+                value: 4,
+                message: 'La cantidad de caracteres debe ser mayor o igual a 4',
+              },
+            })}
+            placeholder="Password"
+          />
+          {errors.password && <p>{errors.password.message || errors.password.type}</p>}
+          <input type="submit" />
         </form>
+      )}
     </>
+  )
 }
-
 
 export default LoginForm
