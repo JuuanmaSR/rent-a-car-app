f0939689ca9c86e8d0118b1db5446c23ed86baa5
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-20 23:58:28 -0300

    Se modifica hook
    Este se modifica para implementar el uso del contexto, para agregar la funcionalidad de cerrar la sesión iniciada y devolver un que maneja el estado esta funcionalidad

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,18 +1,51 @@
+import { useCallback, useContext, useState } from 'react'
+import { useJwt } from 'react-jwt'
 import loginService from 'services/login'
-
+import UserContext from 'context/UserContext'
 const useUser = () => {
   const { jwt, setJwt } = useContext(UserContext)
+  const { decodedToken } = useJwt(jwt)
 
-    const login = ({email, password}) => {
-        loginService({email, password})
-        .then(res => {
-            const {access_token, message} = res
-            access_token? console.log(access_token): console.log(message)
+  const [loginState, setLoginState] = useState({ loading: false, error: false, message: null })
+
+  const login = useCallback(
+    ({ email, password }) => {
+      setLoginState({ loading: true, error: false, message: null })
+      loginService({ email, password })
+        .then((res) => {
+          const { access_token, message } = res
+
+          access_token
+            ? (document.cookie = `token=${access_token}; max-age=${
+                60 * 60 * 24
+              }; samesite=strict;secure`)
+            : setLoginState({ loading: false, error: true, message: message })
+
+          loginState.error
+            ? setLoginState({ loading: false, error: true, message })
+            : setLoginState({ loading: false, error: false, message: message })
           setJwt(access_token)
         })
-    }
-    return {login}
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    [setJwt],
+  )
+
+  const logout = useCallback(() => {
+    document.cookie = `token= ; max-age=0`
     setJwt(null)
+  }, [setJwt])
+
+  return {
+    login,
+    logout,
+    isLoginLoading: loginState.loading,
+    hasLoginMessage: loginState.message,
+    isLogged: Boolean(jwt),
+    user: decodedToken,
+  }
 }
 
 export default useUser
