0e9a1ac5f62525fe09854f2fa842b2ea3ce04ca1
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-20 23:48:46 -0300

    Se modifica configuración

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import jsConfigPaths from 'vite-jsconfig-paths'
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import jsConfigPaths from "vite-jsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), jsConfigPaths()],
