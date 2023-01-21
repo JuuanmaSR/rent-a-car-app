c4a44ebde6ba56c7f23a71a1177698c167ccdec2
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-20 23:53:29 -0300

    Se modifica servicio
    Este se modifica para atrapar excepciones  y enviar un mensaje especifico

--- src/services/login.jsx
+++ src/services/login.jsx
@@ -1,15 +1,20 @@
 const ENDPOINT = 'http://localhost:3000/auth'
 
-const login = async ({email, password}) => {
+const login = async ({ email, password }) => {
+  try {
     const res = await fetch(`${ENDPOINT}/login`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
     })
     return await res.json()
-    
+  } catch (error) {
+    return {
+      message: 'A ocurrido un error',
+    }
+  }
 }
 
 export default login
