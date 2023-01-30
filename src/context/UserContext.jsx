c2a91f9d2c490848fc69923af098f70b03da7ff7
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-01-30 16:28:28 -0300

    Se modifica el contexto
    Este se modifica para guardar los clientes en el mismo y poder obtenerlos de manera global mediante el custom hook de usuarios

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import getCustomers from 'services/getCustomers'
 const Context = React.createContext({})
 
 export function UserContextProvider({ children }) {
-  const [jwt, setJwt] = useState(() => document.cookie.replace('token', ''))
-
-  return <Context.Provider value={{ jwt, setJwt }}>{children}</Context.Provider>
+  const [jwt, setJwt] = useState(() => document.cookie.replace('token=', ''))
+  const [customers, setCustomers] = useState([])
+  useEffect(() => {
+    if (!jwt) setCustomers([])
+    getCustomers({ jwt }).then((customers) => setCustomers(customers))
+  }, [jwt])
+  return (
+    <Context.Provider value={{ jwt, setJwt, customers, setCustomers }}>{children}</Context.Provider>
+  )
 }
 
 export default Context
