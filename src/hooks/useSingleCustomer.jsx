6232031fb0f67275f0bbc1f27fca7766e58ce9e8
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-02-06 15:44:41 -0300

    Se agrega custom hook
    Este hook se agrega para manejar la funcionalidad de obtener un cliente de manera individual

--- src/hooks/useSingleCustomer.jsx
+++ src/hooks/useSingleCustomer.jsx
@@ -1,1 +1,41 @@
+import { useContext, useEffect, useState } from 'react'
+import UserContext from 'context/UserContext'
+import getSingleCustomerSerive from 'services/getSingleCustomer'
+import useCustomer from './useCustomer'
+
+const useSingleCustomer = ({ id }) => {
+  const { jwt } = useContext(UserContext)
   const { customers } = useCustomer()
+  const customerFromCache = () => {
+    if (customers) {
+      return customers.find((customer) => customer.id == id)
+    }
+    return false
+  }
+  const [customer, setCustomer] = useState(customerFromCache)
+  const [customerState, setCustomerState] = useState({
+    isLoading: false,
+    hasError: false,
+  })
+  useEffect(() => {
+    if (!customer) {
+      setCustomerState({
+        loading: true,
+        hasError: false,
+      })
+      getSingleCustomerSerive({ jwt, id })
+        .then((res) => {
+          const { customer } = res
+          setCustomer(customer)
+          setCustomerState({ loading: false, hasError: false })
+        })
+        .catch((error) => {
+          setCustomerState({ loading: false, hasError: true })
+        })
+    }
+  }, [customer, id, jwt])
+
+  return { customer, customerState }
+}
+
+export default useSingleCustomer
