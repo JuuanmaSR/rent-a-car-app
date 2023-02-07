99d6a8020659e6600a4e2ef31e3cf646482d0ff5
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-02-07 01:10:14 -0300

    Se agrega componente
    Este componente se agrega para implementar un spinner de carga que se visualiza mientras se espera la carga de datos

--- src/pages/CustomerUpdateForm/CustomerUpdateForm.jsx
+++ src/pages/CustomerUpdateForm/CustomerUpdateForm.jsx
@@ -4,16 +4,31 @@ import { useLocation } from 'wouter'
 import Form from 'components/CustomerForm/CustomerForm'
 import useUser from 'hooks/useUser'
 import useSingleCustomer from 'hooks/useSingleCustomer'
+import Spinner from 'components/Spinner/Spinner'
 
 const CustomerUpdateForm = ({ params }) => {
   const { isLogged } = useUser()
   const { customer, customerState } = useSingleCustomer({ id: params.id })
+  const { hasError, isLoading } = customerState
   const [_, navigate] = useLocation()
   useEffect(() => {
     if (!isLogged) {
       navigate('/')
     }
   }, [isLogged])
+
+  if (isLoading) {
+    return (
+      <>
+        <Helmet>
+          <title>Cargando... | Rent a car</title>
+        </Helmet>
+        <Spinner />
+      </>
+    )
+  }
+
+  if (!customer) return null
 
   return (
     <>
