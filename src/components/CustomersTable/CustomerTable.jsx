11055b422b0b1f464d2e98ca32c47376aa932e42
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-02-14 01:01:53 -0300

    Se modifica componente
    Este se modifica para implementar ventanas pop-up para mensajes de confirmación y alertas

--- src/components/CustomersTable/CustomerTable.jsx
+++ src/components/CustomersTable/CustomerTable.jsx
@@ -1,6 +1,96 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'wouter'
+import ModalPortal from 'components/Modal/Modal'
+import Confirm from 'components/Confirm/Confirm'
+import Alert from 'components/Alert/Alert'
 import './styles.css'
 
-const CustomersTable = ({ customers }) => {
+const CustomersTable = ({ customers, deleteCustomer, customerState }) => {
+  const [_, navigate] = useLocation()
+  const { loading, hasMessage, successfulAction } = customerState
+  const [showModal, setShowModal] = useState(false)
+  const [confirmOpen, setConfirmOpen] = useState(false)
+  const [customerId, setCustomerId] = useState()
+
+  const handleOpenConfirm = (id) => {
+    setCustomerId(id)
+    setConfirmOpen(true)
+  }
+
+  const handleCloseConfirm = () => {
+    setConfirmOpen(false)
+  }
+
+  const handleConfirm = () => {
+    deleteCustomer(customerId)
+    handleCloseConfirm()
+    setShowModal(true)
+  }
+
+  const handleDetail = (id) => {
+    navigate(`/admin/clientes/detail/${id}`)
+  }
+
+  const handleEdit = (id) => {
+    navigate(`/admin/clientes/editar/${id}`)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowModal(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [successfulAction])
+
+  if (confirmOpen) {
+    return (
+      <>
+        <ModalPortal hasButton={true} onClose={handleCloseConfirm}>
+          <Confirm
+            title={'Eliminar cliente'}
+            content={'Esta seguro que desea eliminar este cliente ?'}
+            onClose={handleCloseConfirm}
+            onConfirm={handleConfirm}
+          />
+        </ModalPortal>
+      </>
+    )
+  }
+
+  if (showModal) {
+    if (hasMessage.message.neutral) {
+      return (
+        <>
+          <ModalPortal hasButton={false}>
+            {loading && <Alert severity={'wait'} children={hasMessage.message.neutral} />}
+          </ModalPortal>
+        </>
+      )
+    }
+    if (hasMessage.message.successful) {
+      return (
+        <>
+          <ModalPortal hasButton={false}>
+            {hasMessage.message.successful && (
+              <Alert severity={'success'} message={hasMessage.message.successful} />
+            )}
+          </ModalPortal>
+        </>
+      )
+    }
+    if (hasMessage.message.failure) {
+      return (
+        <>
+          <ModalPortal hasButton={false}>
+            {hasMessage.message.failure && (
+              <Alert severity={'error'} message={hasMessage.message.failure} />
+            )}
+          </ModalPortal>
+        </>
+      )
+    }
+  }
+
   return (
     <>
       <table className="table-container">
@@ -14,17 +104,17 @@ const CustomersTable = ({ customers }) => {
           </tr>
         </thead>
         <tbody>
-          {customers.map(({ id, firstName, lastName, phoneNumber, address }) => {
+          {customers.map((customer) => {
             return (
-              <tr key={id}>
-                <td>{firstName}</td>
-                <td>{lastName}</td>
-                <td>{phoneNumber}</td>
-                <td>{address}</td>
+              <tr key={customer.id}>
+                <td>{customer.firstName}</td>
+                <td>{customer.lastName}</td>
+                <td>{customer.phoneNumber}</td>
+                <td>{customer.address}</td>
                 <td>
-                  <button>Detalle</button>
-                  <button>Editar</button>
-                  <button>Eliminar</button>
+                  <button onClick={() => handleDetail(customer.id)}>Detalle</button>
+                  <button onClick={() => handleEdit(customer.id)}>Editar</button>
+                  <button onClick={() => handleOpenConfirm(customer.id)}>Eliminar</button>
                 </td>
               </tr>
             )
