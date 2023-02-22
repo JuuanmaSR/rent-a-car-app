ba543dc8374b9a869bd887091216ad4412d7b714
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-02-22 17:21:28 -0300

    Se modifica componente de tablero de vehículos
    Este se modifica para mostrar una pantalla de espera mientras los vehículos son cargados

--- src/pages/CarDashboard/CarDashboard.jsx
+++ src/pages/CarDashboard/CarDashboard.jsx
@@ -3,9 +3,13 @@ import { Helmet } from 'react-helmet'
 import { useLocation } from 'wouter'
 import Dashboard from 'components/Dashboard/Dashboard'
 import useUser from 'hooks/useUser'
+import useCar from 'hooks/useCar'
+import CarsTable from 'components/CarsTable/CarsTable'
+import Spinner from 'components/Spinner/Spinner'
 
 const CarDashboard = () => {
   const { isLogged } = useUser()
+  const { cars } = useCar()
   const [_, navigate] = useLocation()
   useEffect(() => {
     if (!isLogged) {
@@ -18,7 +22,9 @@ const CarDashboard = () => {
       <Helmet>
         <title>Vehiculos | Rent a car</title>
       </Helmet>
-      <Dashboard title="Vehiculos" formRoute={'/'} />
+      <Dashboard title="Vehiculos" formRoute={'/'}>
+        {cars ? <CarsTable cars={cars} /> : <Spinner />}
+      </Dashboard>
     </>
   )
 }
