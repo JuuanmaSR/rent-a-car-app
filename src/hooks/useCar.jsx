a0c8a3696cbfa1e9708d9ec7fdf28e8642d9919d
JuuanmaSR <juan_rider_sp@hotmail.com>
2023-02-25 17:50:07 -0300

    Se modifica custom hook
    Este se modifica para implementar un método  que utilizara la página para agregar nuevos vehículos

--- src/hooks/useCar.jsx
+++ src/hooks/useCar.jsx
@@ -1,11 +1,70 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import addCarService from 'services/addCar'
 import CarContext from 'context/CarContext'
+import UserContext from 'context/UserContext'
 
 const useCar = () => {
-  const { cars } = useContext(CarContext)
+  const { jwt } = useContext(UserContext)
+  const { cars, setCars } = useContext(CarContext)
+  const [carState, setCarState] = useState({
+    loading: false,
+    hasMessage: { value: false, message: { successful: false, failure: false, neutral: false } },
+    successfulAction: false,
+  })
+  const addCar = ({ car }) => {
+    setCarState({
+      loading: true,
+      hasMessage: { value: true, message: { ...{}, neutral: 'Verificando datos...' } },
+    })
+    addCarService({ jwt, data: car })
+      .then((res) => {
+        if (res.statusCode === 400) {
+          setCarState({
+            loading: false,
+            hasMessage: {
+              value: true,
+              message: {
+                neutral: false,
+                successful: false,
+                failure: 'El vehiculo no pudo ser creado ',
+              },
+            },
+            successfulAction: false,
+          })
+        }
+        if (res.car) {
+          setCars((prevCars) => prevCars.concat([res.car]))
+          setCarState({
+            loading: false,
+            hasMessage: {
+              value: true,
+              message: {
+                neutral: false,
+                failure: false,
+                successful: 'El vehículo fue creado con exito',
+              },
+            },
+            successfulAction: true,
+          })
+        }
+      })
+      .catch((error) => {
+        setCarState({
+          loading: false,
+          hasMessage: {
+            value: true,
+            message: { failure: 'A ocurrido un error', successful: false, neutral: false },
+          },
+          successfulAction: false,
+        })
+        console.error(error)
+      })
+  }
 
   return {
     cars,
+    addCar,
+    carState,
   }
 }
 
